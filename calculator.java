3a446cf469a3a472afc8a409326ae71fd2485524
Abhishek jat <abhishekjat80094@gmail.com>
2025-01-06 16:54:56 +0530

    addition operation

--- calculator.java
+++ calculator.java
@@ -2,9 +2,18 @@ import java.util.*;
 
 class calculator
 {
+    public static int addition(int number1, int number2)
+    {
+        return number1 + number2;
+    }
     public static void main(String [] args)
     {
-
         System.out.println("Welcome to the calculator");
+        Scanner sc = new Scanner(System.in);
+        int num1 = sc.nextInt();
+        int num2 = sc.nextInt();
+
+        int sum = addition(num1, num2);
+        System.out.println("The sum of " + num1 + " and " + num2 + " is " + sum);
     }
 }
