f744e5ac2fac4c778b8262d0d21563109cb939d2
Abhishek jat <abhishekjat80094@gmail.com>
2025-01-06 23:08:18 +0530

    multiplication done

--- calculator.java
+++ calculator.java
@@ -10,6 +10,10 @@ class calculator
     {
         return number1 - number2;
     }
+    public static int multiply(int number1, int number2)
+    {
+        return number1 * number2;
+    }
     public static void main(String [] args)
     {
         System.out.println("Welcome to the calculator");
@@ -19,7 +23,10 @@ class calculator
 
         int sum = addition(num1, num2);
         int sub = substraction(num1, num2);
+        int mul = multiply(num1, num2);
         System.out.println("The sum of " + num1 + " and " + num2 + " is " + sum);
         System.out.println("The difference of " + num1 + " and " + num2 + " is " + sub);
+        System.out.println("The multiplication of " + num1 + " and " + num2 + " is " + mul);
+        
     }
 }
