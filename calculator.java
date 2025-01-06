14447b81427887fe154f100298e778c490661a8f
Abhishek jat <abhishekjat80094@gmail.com>
2025-01-06 22:31:42 +0530

    substraction added

--- calculator.java
+++ calculator.java
@@ -6,6 +6,10 @@ class calculator
     {
         return number1 + number2;
     }
+    public static int substraction(int number1, int number2)
+    {
+        return number1 - number2;
+    }
     public static void main(String [] args)
     {
         System.out.println("Welcome to the calculator");
@@ -14,6 +18,8 @@ class calculator
         int num2 = sc.nextInt();
 
         int sum = addition(num1, num2);
+        int sub = substraction(num1, num2);
         System.out.println("The sum of " + num1 + " and " + num2 + " is " + sum);
+        System.out.println("The difference of " + num1 + " and " + num2 + " is " + sub);
     }
 }
